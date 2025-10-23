71ba26fa04d0cb154923c66c2efe1f8982982014
Nassim Azzouzi <Nassim_Azzouzi01@etu.ube.fr>
2025-10-23 10:02:06 +0200

    feat : Première implémentation de la map pour une vile

--- frontend/src/features/Villes/ville.tsx
+++ frontend/src/features/Villes/ville.tsx
@@ -1,9 +1,19 @@
-export default function Ville() {
-  
+import CityMap from './cityMap';
+import villesData from './villesData.json';
 
-  return (
-    <div className="text-white bg-[#103035] p-4 rounded-lg">
-      <h2>Rome</h2>
-    </div>
+const getCityFeature = (cityName: string) => {
+  return villesData.features.find(
+    f => f.properties?.name?.toLowerCase() === cityName.toLowerCase()
   );
-}
+};
+
+const Ville = () => {
+  const cityName = 'Rome'; // Tu peux le rendre dynamique plus tard
+  const feature = getCityFeature(cityName);
+
+  if (!feature) return <p>Ville "{cityName}" non trouvée.</p>;
+
+  return <CityMap feature={feature} />;
+};
+
+export default Ville;
--- frontend/src/features/home/HomePage.tsx
+++ frontend/src/features/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from 'react'; 
 import PlaneImage from '../../assets/avion-deco.png';
 import { Link } from 'react-router-dom';
 export default function HomePage() {
