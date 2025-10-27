76b7483024c0b547056d14820f42b4c1d3d89981
Nassim Azzouzi <Nassim_Azzouzi01@etu.ube.fr>
2025-10-28 00:45:35 +0100

    feat : nouvelle page résultats de recherche et un composant d'un produit afficher dans ces résultats

--- frontend/src/features/Recherche/Resultats.tsx
+++ frontend/src/features/Recherche/Resultats.tsx
@@ -5,7 +5,7 @@ import Left_ico from '../../assets/Left_Ico.svg'
 
 import Train_Ico from '../../assets/train_ico2.svg';
 import Plane_Ico from '../../assets/plane_ico2.svg';
-
+import Productcard from './ProductCard/ProductCard.tsx';
 
 
 export default function Resultats ()
@@ -34,17 +34,54 @@ export default function Resultats ()
                 <img src={Right_ico} alt="" className='ml-2'/>
             </button>
         </div>
-        <div className="flex items-center justify-between px-4">
-            <button className="bg-[#133A40] w-1/2 flex justify-center items-center p-2 rounded-md">
+        <div className="flex items-center justify-between w-100 -ml-4 m-10 ">
+            <button className="bg-[#133A40] w-2/3 h-17 flex justify-center items-center p-2 rounded-tr-3xl border-b-[#98EAF3] border-b-4">
+                <div >
                 <img src={Train_Ico} alt="Train" className="" />
+                <span className='text-[#98EAF3] -ml-1'> 10,50 €</span>
+                </div>
             </button>
 
-            <button className="ml-4 p-2 rounded-md">
+            <button className="w-2/3 h-17 flex justify-center items-center border-b-white rounded-tl-3xl border-b-4 ">
                 <img src={Plane_Ico} alt="Avion" className="" />
             </button>
 
 
         </div>
+        <div className="bg-[#133A40] px-4 pt-5 -mt-10 w-98 h-300 -ml-4 ">
+            <div className="flex  gap-2  -ml-3">
+                <button className="flex items-center gap-1 border-[#98EAF3] border-2 px-4 py-2 rounded-full text-[#98EAF3]  rounded-full text-sm w-24">
+                <span className='-ml-1'>Horaires </span>
+                <span className="text-[#98EAF3]">▼</span>
+                </button>
+
+               
+                <button className="flex items-left  gap-1 border-[#98EAF3] border-2 px-4 py-2 rounded-full text-[#98EAF3] px-4 py-2 rounded-full text-sm w-20">
+                <span className='-ml-1'>Gares </span>
+                <span className="text-[#98EAF3]">▼</span>
+                </button>
+
+                <button className="flex items-center gap-1 border-[#98EAF3] border-2 px-4 py-2 rounded-full text-[#98EAF3] text-[#98EAF3] px-4 py-2 rounded-full text-sm w-24">
+                <span className='-ml-1'>Départs </span>
+                <span className="text-[#98EAF3]">▼</span>
+                </button>
+
+                <button className="flex items-center gap-2 text-[#133A40] bg-[#98EAF3] px-4 py-2 rounded-full text-sm w-20">
+                <span className='-ml-1'>Direct</span>
+                <span className="text-[#133A40]">▼</span>
+                </button>
+            </div>
+            <Productcard/>
+            <Productcard/>
+            <Productcard/>
+            <Productcard/>
+            <Productcard/>
+            <Productcard/>
+        
+        </div>
+
+
+    
 
 
         </>
