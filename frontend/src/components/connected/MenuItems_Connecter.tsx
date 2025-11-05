76095ba43c9c1823c2739a504c4711499cd4f4d6
Nassim Azzouzi <Nassim_Azzouzi01@etu.ube.fr>
2025-11-05 15:47:50 +0100

    Fix : Frontend du menu humberger fini !

--- frontend/src/components/connected/MenuItems_Connecter.tsx
+++ frontend/src/components/connected/MenuItems_Connecter.tsx
@@ -6,14 +6,14 @@ import Carte_Reduc_Ico from '../../assets/Carte_Reduc_Ico.svg'
 export default function Menu_items_Connecter ()
 {
     return (
-        <div className="flex display-flex border-b-3 border-[#4A6367] w-80 ml-10  mt-10">
-            <div className="grid grid-cols gap-8  mb-5  mt-2 w-10">
+        <div className="flex display-flex border-b-3 border-[#4A6367] w-85 ml-7  mt-6">
+            <div className="grid grid-cols gap-5  mb-5  mt-2 w-10">
                 <img src={Reservation_ico} alt="" />
                 <img src={Panier_Ico} alt="" />
                 <img src={Carte_Reduc_Ico} alt="" />
 
             </div>
-            <div className="grid grid-cols gap-8  w-full items-left mb-5">
+            <div className="grid grid-cols gap-5  w-full items-left mb-5">
                 <Link to="/" className="text-left font-bold text-xl"> <span>Mes Reservations</span> </Link>
                 <Link to="/" className="text-left text-xl font-bold"> Panier </Link>
                 <Link to="/" className="text-left font-bold text-xl ">Mes cartes de réduction </Link>
