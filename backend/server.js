6c6376dd396e19b4b0fb469f604d728e8e848856
André <111359909+A-thepuma@users.noreply.github.com>
2025-11-14 10:39:29 +0100

    fix: server.js ajout dans ALLOWED

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ const ALLOWED = [
   "http://127.0.0.1:5173",
   "http://localhost:3000",
   "http://127.0.0.1:3000",
+  "https://horizons-plus-production.up.railway.app",
 ];
 
 const corsOptions = {
