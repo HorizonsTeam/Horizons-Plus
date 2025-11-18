d64c80d4f39626a3eeb28d40dfed568771c24993
André <111359909+A-thepuma@users.noreply.github.com>
2025-11-18 11:46:52 +0100

    fix : fronted apptx mot de passe oublié

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,9 +9,9 @@ import MdpOublie from './features/connexion/MdpOublie/mdpoublie.tsx';
 import Panier from './features/panier/panier.tsx';
 import Resultats from './features/Recherche/Resultats.tsx';
 import Recap from './features/Products/recap.tsx';
-import PaymentPage  from './features/Products/Billets/Payment.tsx';
+import PaymentPage from './features/Products/Billets/Payment.tsx';
 
-import Infos_Passagers from'./features/Products/Billets/Infos_Passagers.tsx';
+import Infos_Passagers from './features/Products/Billets/Infos_Passagers.tsx';
 
 
 
@@ -23,6 +23,7 @@ function App() {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
           <Route path="login" element={<Login />} />
+          <Route path="mdpoublie" element={<MdpOublie />} />
           <Route path="singin" element={<Singin />} />
           <Route path="panier" element={<Panier />} />
           <Route path="Recherche" element={<Resultats />} />
@@ -30,11 +31,10 @@ function App() {
           <Route path="PaymentPage" element={<PaymentPage />} />
           <Route path="Infos_Passagers" element={<Infos_Passagers />} />
 
-           
 
 
         </Route>
-        
+
       </Routes>
     </BrowserRouter>
   );
