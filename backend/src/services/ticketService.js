02c92ed50a3f0160a5e5fc5955def50fee6a0953
André <111359909+A-thepuma@users.noreply.github.com>
2025-12-11 23:49:06 +0100

    fix : piece jointe pdf

--- backend/src/services/ticketService.js
+++ backend/src/services/ticketService.js
@@ -1,13 +1,12 @@
-import path from "path";
 import { generateQR } from "./qrService.js";
 import { generatePDF } from "./pdfService.js";
 
 export async function generateTicketPDF(info) {
     const qrBase64 = await generateQR(info.ticketId);
 
-    const outputPath = path.resolve(`tickets/ticket-${info.ticketId}.pdf`);
+    const pdfBytes = await generatePDF(info, qrBase64);
 
     await generatePDF(info, qrBase64, outputPath);
 
-    return outputPath;
+    return pdfBytes;
 }
